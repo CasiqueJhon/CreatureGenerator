8eee20200a8bbc13343c03dfdb6438ecb962d794
jhon.casiquegarcia <jhoncasique04@hotmail.com>
2020-01-26 10:43:13 +0100

    PresenterTest

--- app/src/test/java/com/raywenderlich/android/creaturemon/presenter/CreaturePresenterTest.kt
+++ app/src/test/java/com/raywenderlich/android/creaturemon/presenter/CreaturePresenterTest.kt
@@ -1,9 +1,57 @@
 package com.raywenderlich.android.creaturemon.presenter
 
-import org.junit.Assert.*
+import com.raywenderlich.android.creaturemon.model.*
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.*
+import org.mockito.MockitoAnnotations
 
 class CreaturePresenterTest {
+
     private lateinit var presenter: CreaturePresenter
 
+    @Mock
+    lateinit var repository: CreatureRepository
 
+    @Mock
+    lateinit var view: CreatureContract.View
+
+    @Mock
+    lateinit var generator: CreatureGenerator
+
+    @Before
+    fun setup() {
+        MockitoAnnotations.initMocks(this)
+        presenter = CreaturePresenter(generator, repository)
+        presenter.setView(view)
+    }
+
+    @Test
+    fun testIntelligenceSelected() {
+        val attributes = CreatureAttributes(10, 0, 0)
+        val creatureGenerated = Creature(attributes, 50)
+        `when`(generator.creatureGenerated(attributes)).thenReturn(creatureGenerated)
+        presenter.attributeSelected(AttributeType.INTELLIGENCE, 3)
+        verify(view, times(1)).showHitPoints("50")
+    }
+
+    @Test
+    fun testStrengthSelected() {
+        val attributes = CreatureAttributes(0, 10, 0)
+        val creatureGenerated = Creature(attributes, 30)
+        `when`(generator.creatureGenerated(attributes)).thenReturn(creatureGenerated)
+        presenter.attributeSelected(AttributeType.STRENGTH, 3)
+        verify(view, times(1)).showHitPoints("30")
+    }
+
+    @Test
+    fun testEnduranceSelected() {
+        val attributes = CreatureAttributes(0, 0, 10)
+        val creatureGenerated = Creature(attributes, 40)
+        `when`(generator.creatureGenerated(attributes)).thenReturn(creatureGenerated)
+        presenter.attributeSelected(AttributeType.ENDURANCE, 3)
+        verify(view, times(1)).showHitPoints("40")
+    }
 }
