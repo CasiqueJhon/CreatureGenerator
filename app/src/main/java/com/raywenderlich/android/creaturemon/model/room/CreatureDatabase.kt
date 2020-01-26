25cde7b522fedcc535101cde12adaf229c84f386
jhon.casiquegarcia <jhoncasique04@hotmail.com>
2020-01-26 11:04:05 +0100

    PresenterTest

--- app/src/main/java/com/raywenderlich/android/creaturemon/model/room/CreatureDatabase.kt
+++ app/src/main/java/com/raywenderlich/android/creaturemon/model/room/CreatureDatabase.kt
@@ -2,9 +2,11 @@ package com.raywenderlich.android.creaturemon.model.room
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.raywenderlich.android.creaturemon.model.Creature
 
 @Database(entities = [(Creature::class)], version = 1)
+@TypeConverters(CreatureAttributesConverter::class)
 abstract class CreatureDatabase : RoomDatabase() {
   abstract fun creatureDao(): CreatureDao
 }
